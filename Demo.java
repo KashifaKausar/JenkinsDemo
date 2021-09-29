1b39dfc13383bca665f40fd964a649cf18dee9af
KashifaKausar <91588627+KashifaKausar@users.noreply.github.com>
2021-09-29 13:06:31 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Demo{
+public static void main(String[] args){
+System.out.println("Hi.This is demo for jenkins");
+System.out.println("Hi.This is demo for jenkins");
+}
+}
